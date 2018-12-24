d5554f88fd182fdd17fc113a13c3d2d86568c5ac
BQ Lab <beaqon@naver.com>
2018-12-24 11:10:39 +0900

    Make intro and noActionBar theme

--- app/src/main/java/app/bqlab/vestband/LoginActivity.java
+++ app/src/main/java/app/bqlab/vestband/LoginActivity.java
@@ -1,10 +1,7 @@
 package app.bqlab.vestband;
 
-import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,29 +9,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        init();
-    }
-
-    private void init() {
-        findViewById(R.id.login_login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showLoginDialog();
-            }
-        });
-        findViewById(R.id.login_register).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showRegisterDialog();
-            }
-        });
-    }
-
-    private void showRegisterDialog() {
-        String name, sex, id, pw, email;
-        ListView
-        new AlertDialog.Builder(this)
-                .setTitle("회원가입")
-
     }
 }
