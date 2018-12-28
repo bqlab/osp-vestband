f4887c9dc62c0dc858f9eb72d832dc064b27c974
BQ Lab <beaqon@naver.com>
2018-12-29 01:05:29 +0900

    I need a rest

--- app/src/main/java/app/bqlab/vestband/DashboardActivity.java
+++ app/src/main/java/app/bqlab/vestband/DashboardActivity.java
@@ -1,15 +1,13 @@
 package app.bqlab.vestband;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class IntroActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
-        startActivity(new Intent(this, DashboardActivity.class));
+        setContentView(R.layout.activity_dashboard);
     }
 }
