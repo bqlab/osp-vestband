24fc0e447df7c575544add4cef6d1b2cdd24f4b3
BQ Lab <beaqon@naver.com>
2018-12-31 23:53:43 +0900

    -

--- app/src/main/java/app/bqlab/vestband/StartActivity.java
+++ app/src/main/java/app/bqlab/vestband/StartActivity.java
@@ -1,18 +1,18 @@
 package app.bqlab.vestband;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     String
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_start);
         init();
     }
 
@@ -20,7 +20,7 @@ public class LoginActivity extends AppCompatActivity {
         findViewById(R.id.login_main_bot_register).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
             }
         });
         findViewById(R.id.login_main_bot_login).setOnClickListener(new View.OnClickListener() {
