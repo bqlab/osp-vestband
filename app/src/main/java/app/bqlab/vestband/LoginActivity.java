0f41d19d7476202380bb1d40255c464bd39f35d0
BQ Lab <beaqon@naver.com>
2019-01-01 02:06:49 +0900

    Need to sleep, refer to the comment

--- app/src/main/java/app/bqlab/vestband/LoginActivity.java
+++ app/src/main/java/app/bqlab/vestband/LoginActivity.java
@@ -1,9 +1,14 @@
 package app.bqlab.vestband;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -14,17 +19,50 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void init() {
-        findViewById(R.id.login_main_bot_register).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.login_find).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                //make pop-up
             }
         });
-        findViewById(R.id.login_main_bot_login).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.login_done).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.login_main).setVisibility(View.GONE);
-                findViewById(R.id.login_login).setVisibility(View.GONE);
+                String id = ((EditText) findViewById(R.id.login_id)).getText().toString();
+                String pw = ((EditText) findViewById(R.id.login_pw)).getText().toString();
+
+                if (!id.contains("@") || id.isEmpty() || pw.isEmpty()) {
+                    new AlertDialog.Builder(LoginActivity.this)
+                            .setMessage("아이디와 비밀번호를 다시 확인하세요.")
+                            .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).show();
+                } else {
+                    if (!pw.equals(getSharedPreferences("idpw", MODE_PRIVATE).getString(id, "none"))) {
+                        new AlertDialog.Builder(LoginActivity.this)
+                                .setMessage("아이디와 비밀번호를 다시 확인하세요.")
+                                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .setNeutralButton("회원가입", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                                    }
+                                }).show();
+                    } else {
+                        if (getSharedPreferences("flag", MODE_PRIVATE).getBoolean("beginner", true)) {
+                            getSharedPreferences("flag", MODE_PRIVATE).edit().putBoolean("beginner", false).apply();
+                            startActivity(new Intent(LoginActivity.this, InitialActivity.class));
+                        }
+                    }
+                }
             }
         });
     }
--- app/src/main/java/app/bqlab/vestband/PwsearchLayout.java
+++ app/src/main/java/app/bqlab/vestband/PwsearchLayout.java
@@ -1,4 +1,13 @@
 package app.bqlab.vestband;
 
-public class PwsearchLayout {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class PwsearchLayout extends LinearLayout {
+
+    public PwsearchLayout(Context context) {
+        super(context);
+        LayoutInflater.from(getContext()).inflate(R.layout.layout_pwsearch, this);
+    }
 }
