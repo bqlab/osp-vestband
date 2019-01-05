4a74c16a790bc6c4a9762dd8a78525ee188d48c2
BQ Lab <beaqon@naver.com>
2019-01-05 11:34:32 +0900

    Made notify service

--- app/src/main/java/app/bqlab/vestband/UserService.java
+++ app/src/main/java/app/bqlab/vestband/UserService.java
@@ -1,10 +1,14 @@
 package app.bqlab.vestband;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
@@ -13,6 +17,9 @@ import android.util.Log;
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
 
 public class UserService extends Service {
+    NotificationManager notificationManager;
+    NotificationChannel notificationChannel;
+
     public static String id;
     public static int degree = 0;
     public static boolean isConnected = false;
@@ -25,7 +32,21 @@ public class UserService extends Service {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        String content = intent.getStringExtra("content");
+        Intent i = new Intent(this, MainActivity.class);
+        PendingIntent p = PendingIntent.getActivity(this, 0, i, 0);
+        Notification notification = new NotificationCompat.Builder(this, "알림")
+                .setContentText(content)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(p)
+                .build();
+        startForeground(1, notification);
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -46,5 +67,42 @@ public class UserService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public void checkAndroidVersion() {
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = new NotificationChannel("em", "긴급알림", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("긴급한 상황을 알립니다.");
+            notificationChannel.enableLights(true);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 100, 200});
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    public void makeNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.notify(0, new NotificationCompat.Builder(this, "em")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("알림")
+                    .setContentText("현재 나쁜 자세를 취하고 있습니다.")
+                    .setWhen(System.currentTimeMillis())
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .build());
+        } else {
+            notificationManager.notify(0, new Notification.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("알림")
+                    .setContentText("현재 나쁜 자세를 취하고 있습니다.")
+                    .setWhen(System.currentTimeMillis())
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .build());
+        }
     }
 }
